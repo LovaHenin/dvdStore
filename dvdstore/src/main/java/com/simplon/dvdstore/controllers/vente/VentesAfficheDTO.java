a21e5bac0694e51e10f2d0d55a6bbfc6721ae3c2
LovaHenin <ratovolova@yahoo.com>
2023-10-12 22:47:38 +0200

    correctif sur venteById

--- dvdstore/src/main/java/com/simplon/dvdstore/controllers/vente/VentesAfficheDTO.java
+++ dvdstore/src/main/java/com/simplon/dvdstore/controllers/vente/VentesAfficheDTO.java
@@ -1,6 +1,7 @@
 package com.simplon.dvdstore.controllers.vente;
 
+import com.simplon.dvdstore.controllers.client.ClientAfficheDTO;
 import com.simplon.dvdstore.controllers.client.ClientDTO;
 import com.simplon.dvdstore.controllers.dvd.DvdStoreDTO;
 
-public record VentesAfficheDTO(Long id, String date, int quantite, Float prix, DvdStoreDTO dvdStoreDTO, ClientDTO clientDTO) {}
+public record VentesAfficheDTO(Long id, String date, int quantite, Float prix, DvdStoreDTO dvdStoreDTO, ClientAfficheDTO clientAfficheDTO) {}
---
+++ dvdstorePostgre/src/main/java/com/simplon/dvdstorePostgre/DvdstorePostgreApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.dvdstorePostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdstorePostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdstorePostgreApplication.class, args);
+	}
+
+}
